602c024b2b696a68547f767ae61f4397e6e83738
edissarda17414 <edissarda@yahoo.com>
2016-03-19 13:03:12 +0100

    Implementirana metoda dodajKursValute u klasi Menjacnica u master grani

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,6 +1,7 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 import menjacnica.valuta.Kurs;
@@ -8,10 +9,28 @@ import menjacnica.valuta.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	
+	private LinkedList<Valuta> valute = new LinkedList<>();
+	
 	@Override
-	public void dodajKursValute(Valuta valuta, double prodajni, double kupovni, double srednji,
-			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	public void dodajKursValute(Valuta valuta, double prodajni, double kupovni, double srednji, GregorianCalendar datum) {
+		
+		Kurs kurs = new Kurs();
+		kurs.setDatum(datum);
+		kurs.setKupovni(kupovni);
+		kurs.setProdajni(prodajni);
+		kurs.setSrednji(srednji);
+		
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v = valute.get(i);
+			
+			if (v.equals(valuta)) {
+				if (!v.getKursevi().contains(kurs)) {
+					v.getKursevi().add(kurs);
+					break;
+				}
+			}
+		}
 
 	}
 
